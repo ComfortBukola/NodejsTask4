94d564027ceddf47ec50a363ecf8ebbc668debf9
ComfortBukola <61964458+ComfortBukola@users.noreply.github.com>
2020-05-04 14:06:41 +0100

    Create authRoutes.js

---
+++ authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Users = require('../controllers/userController');
+
+const router = express.Router();
+
+router.route('/signup').post(Users.createUser);
+router.route('/signin').post(Users.signin);
+router.route('/tutors').get(Users.findTutors);
+module.exports = router;
